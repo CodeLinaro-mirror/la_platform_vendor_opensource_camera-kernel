30f2983d341d914df29e01d0a280f41d530d1ab7
Shivakumar Malke <quic_smalke@quicinc.com>
2023-05-19 11:52:42 +0530

    msm: camera: mem_mgr: Add refcount to track in use buffers
    The function cam_mem_mgr_release can unmap the buffers when in use.
    
    This change prevents unmapping the buffers when in use.
    
    CRs-Fixed: 3489559
    Change-Id: I2e72e795d39ac15abfa56c19043c419a03686966
    Signed-off-by: Shivakumar Malke <quic_smalke@quicinc.com>
    Signed-off-by: Gaurav Jindal <quic_gjindal@quicinc.com>

--- drivers/cam_isp/cam_isp_context.c
+++ drivers/cam_isp/cam_isp_context.c
@@ -986,6 +986,7 @@ static int cam_isp_ctx_dump_req(
 				CAM_ERR(CAM_ISP,
 					"Invalid offset exp %u actual %u",
 					req_isp->cfg[i].offset, (uint32_t)len);
+				cam_mem_put_cpu_buf(req_isp->cfg[i].handle);
 				return -EINVAL;
 			}
 			remain_len = len - req_isp->cfg[i].offset;
@@ -996,6 +997,7 @@ static int cam_isp_ctx_dump_req(
 					"Invalid len exp %u remain_len %u",
 					req_isp->cfg[i].len,
 					(uint32_t)remain_len);
+				cam_mem_put_cpu_buf(req_isp->cfg[i].handle);
 				return -EINVAL;
 			}
 
@@ -1021,6 +1023,7 @@ static int cam_isp_ctx_dump_req(
 					return rc;
 			} else
 				cam_cdm_util_dump_cmd_buf(buf_start, buf_end);
+			cam_mem_put_cpu_buf(req_isp->cfg[i].handle);
 		}
 	}
 	return rc;
@@ -5290,6 +5293,7 @@ hw_dump:
 		mutex_unlock(&ctx_isp->isp_mutex);
 		CAM_WARN(CAM_ISP, "Dump buffer overshoot len %zu offset %zu",
 			buf_len, dump_info->offset);
+		cam_mem_put_cpu_buf(dump_info->buf_handle);
 		return -ENOSPC;
 	}
 
@@ -5298,9 +5302,10 @@ hw_dump:
 		(CAM_ISP_CTX_DUMP_NUM_WORDS * sizeof(uint64_t));
 
 	if (remain_len < min_len) {
-		mutex_unlock(&ctx_isp->isp_mutex);
 		CAM_WARN(CAM_ISP, "Dump buffer exhaust remain %zu min %u",
 			remain_len, min_len);
+		cam_mem_put_cpu_buf(dump_info->buf_handle);
+		mutex_unlock(&ctx_isp->isp_mutex);
 		return -ENOSPC;
 	}
 
@@ -5339,10 +5344,12 @@ hw_dump:
 	if (rc) {
 		CAM_ERR(CAM_ISP, "Dump event fail %lld",
 			req->request_id);
+		cam_mem_put_cpu_buf(dump_info->buf_handle);
 		mutex_unlock(&ctx_isp->isp_mutex);
 		return rc;
 	}
 	if (dump_only_event_record) {
+		cam_mem_put_cpu_buf(dump_info->buf_handle);
 		mutex_unlock(&ctx_isp->isp_mutex);
 		return rc;
 	}
@@ -5351,6 +5358,7 @@ hw_dump:
 	if (rc) {
 		CAM_ERR(CAM_ISP, "Dump Req info fail %lld",
 			req->request_id);
+		cam_mem_put_cpu_buf(dump_info->buf_handle);
 		mutex_unlock(&ctx_isp->isp_mutex);
 		return rc;
 	}
@@ -5366,6 +5374,7 @@ hw_dump:
 			&dump_args);
 		dump_info->offset = dump_args.offset;
 	}
+	cam_mem_put_cpu_buf(dump_info->buf_handle);
 	return rc;
 }
 
