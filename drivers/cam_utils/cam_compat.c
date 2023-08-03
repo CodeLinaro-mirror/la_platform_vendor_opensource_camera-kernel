7afc1a57153a92751b910c0c5d55df94624aefa7
Stuti Saxena <quic_stutsaxe@quicinc.com>
2023-08-03 11:50:21 +0530

    msm: camera: common: Change dma_buf_map to iosys_map
    Struct dma_buf_map change between kernel version
    5.15 to 5.18.
    
    CRs-Fixed: 3619048
    Change-Id: I29437ba64175a07096f55bbc0fceef5b777de6ce
    Signed-off-by: Stuti Saxena <quic_stutsaxe@quicinc.com>

--- drivers/cam_utils/cam_compat.c
+++ drivers/cam_utils/cam_compat.c
@@ -479,7 +479,49 @@ static int inline cam_subdev_list_cmp(struct cam_subdev *entry_1, struct cam_sub
 		return 0;
 }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+#if (KERNEL_VERSION(5, 18, 0) <= LINUX_VERSION_CODE)
+int cam_compat_util_get_dmabuf_va(struct dma_buf *dmabuf, uintptr_t *vaddr)
+{
+	struct iosys_map mapping;
+	int error_code = dma_buf_vmap(dmabuf, &mapping);
+
+	if (error_code) {
+		*vaddr = 0;
+	} else {
+		*vaddr = (mapping.is_iomem) ?
+			(uintptr_t)mapping.vaddr_iomem :
+			(uintptr_t)mapping.vaddr;
+		CAM_DBG(CAM_MEM,
+				"dmabuf=%p, *vaddr=%p, is_iomem=%d, vaddr_iomem=%p,
+				vaddr=%p",
+				dmabuf, *vaddr, mapping.is_iomem, mapping.vaddr_iomem,
+				mapping.vaddr);
+	}
+
+	return error_code;
+}
+
+void cam_compat_util_put_dmabuf_va(struct dma_buf *dmabuf, void *vaddr)
+{
+	struct iosys_map mapping = IOSYS_MAP_INIT_VADDR(vaddr);
+
+	dma_buf_vunmap(dmabuf, &mapping);
+}
+
+int cam_req_mgr_ordered_list_cmp(void *priv,
+	const struct list_head *head_1, const struct list_head *head_2)
+{
+	return cam_subdev_list_cmp(list_entry(head_1, struct cam_subdev, list),
+		list_entry(head_2, struct cam_subdev, list));
+}
+
+void cam_smmu_util_iommu_custom(struct device *dev,
+	dma_addr_t discard_start, size_t discard_length)
+{
+	return;
+}
+
+#elif (KERNEL_VERSION(5, 15, 0) <= LINUX_VERSION_CODE)
 void cam_smmu_util_iommu_custom(struct device *dev,
 	dma_addr_t discard_start, size_t discard_length)
 {
