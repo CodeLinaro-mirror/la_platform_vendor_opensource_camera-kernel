8239b92dd88946a26f4770c931c713d8741bab07
Vishal Verma <quic_vishverm@quicinc.com>
2024-09-19 15:58:27 +0530

    msm: camera: sensor: enable sensor config for QCS8300
    -- Enable config flag for sensor and its compilation.
    
    CRs-Fixed: 3948089
    Change-Id: Ic1ce85fa0c18229858e5f90b69bddcb905ccbe55
    Signed-off-by: Vishal Verma <quic_vishverm@quicinc.com>

--- config/qcs8300-camera.mk
+++ config/qcs8300-camera.mk
@@ -1,13 +1,13 @@
 # Localized KCONFIG settings
 CONFIG_SPECTRA_ISP := y
 CONFIG_SPECTRA_ICP := y
-# CONFIG_SPECTRA_SENSOR := y
+CONFIG_SPECTRA_SENSOR := y
 CONFIG_SPECTRA_SECURE_CAMNOC_REG_UPDATE := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_SPECTRA_ISP=1
 ccflags-y += -DCONFIG_SPECTRA_ICP=1
-# ccflags-y += -DCONFIG_SPECTRA_SENSOR=1
+ccflags-y += -DCONFIG_SPECTRA_SENSOR=1
 ccflags-y += -DCONFIG_SPECTRA_SECURE_CAMNOC_REG_UPDATE=1
 
 # External Dependencies
