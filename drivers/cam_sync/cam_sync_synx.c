f198f9ff0063d2a00a7f4f866ebdc39918a70a58
Stuti Saxena <quic_stutsaxe@quicinc.com>
2024-10-08 12:31:02 +0530

    msm: camera: sync: Update timeout for synx async wait
    With the update in the synx driver for async wait, clients
    are expected to register the callback with no specific
    timeout value.
    
    CRs-Fixed: 3943277
    Change-Id: Ie9a5a88f9fddc9512b249919c843e2367de7db4e
    Signed-off-by: Karthik Anantha Ram <quic_kartanan@quicinc.com>
    Signed-off-by: Stuti Saxena <quic_stutsaxe@quicinc.com>

--- drivers/cam_sync/cam_sync_synx.c
+++ drivers/cam_sync/cam_sync_synx.c
@@ -494,6 +494,7 @@ int cam_synx_obj_register_cb(int32_t *sync_obj, int32_t row_idx,
 	cb_params.userdata = row;
 	cb_params.cancel_cb_func = NULL;
 	cb_params.h_synx = row->synx_obj;
+	cb_params.timeout_ms = SYNX_NO_TIMEOUT;
 	cb_params.cb_func = __cam_synx_obj_signal_cb;
 
 	rc = synx_async_wait(g_cam_synx_obj_dev->session_handle, &cb_params);
