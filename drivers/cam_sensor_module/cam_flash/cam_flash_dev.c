630315aafa69d8b51190a188f5247f6d85ef9cda
Parvathi K <parvathi@qti.qualcomm.com>
2024-10-31 21:12:42 +0800

    msm: camera: flash: Add current query support for upstream pmic led
    This change adds support to query maximum available
    flash current via a private kernel API which bases on
    battery power supply status.
    
    CRs-Fixed: 3977259
    Change-Id: Iabfbb82632e0eba1f27e42bd6c77d000654a7921
    Signed-off-by: Stark Lin <quic_starlin@quicinc.com>
    Signed-off-by: Nirmal Abraham <c_nabrah@qti.qualcomm.com>
    Signed-off-by: Parvathi K <parvathi@qti.qualcomm.com>

--- drivers/cam_sensor_module/cam_flash/cam_flash_dev.c
+++ drivers/cam_sensor_module/cam_flash/cam_flash_dev.c
@@ -10,6 +10,36 @@
 #include "cam_flash_core.h"
 #include "cam_common_util.h"
 
+static void cam_flash_populate_query_current(struct cam_flash_ctrl *fctrl,
+	struct cam_flash_query_cap_info *flash_cap,
+	struct cam_flash_private_soc *soc_private)
+{
+	int i = 0;
+
+	for (i = 0; i < fctrl->flash_num_sources; i++) {
+#if IS_REACHABLE(CONFIG_LEDS_QCOM_FLASH)
+		/* Values got from DT are kept in ua/us, need conversion here. */
+		flash_cap->max_current_flash[i] =
+			soc_private->flash_max_current[i] / UA_PER_MA;
+		flash_cap->max_duration_flash[i] =
+			soc_private->flash_max_duration[i] / US_PER_MS;
+		flash_cap->max_current_torch[i] =
+			soc_private->torch_max_current[i] / UA_PER_MA;
+	}
+#elif __or(IS_REACHABLE(CONFIG_LEDS_QPNP_FLASH_V2), \
+			IS_REACHABLE(CONFIG_LEDS_QTI_FLASH))
+		flash_cap->max_current_flash[i] =
+			soc_private->flash_max_current[i];
+		flash_cap->max_duration_flash[i] =
+			soc_private->flash_max_duration[i];
+	}
+
+	for (i = 0; i < fctrl->torch_num_sources; i++)
+		flash_cap->max_current_torch[i] =
+			soc_private->torch_max_current[i];
+#endif
+}
+
 static int32_t cam_flash_driver_cmd(struct cam_flash_ctrl *fctrl,
 		void *arg, struct cam_flash_private_soc *soc_private)
 {
@@ -140,19 +170,8 @@ static int32_t cam_flash_driver_cmd(struct cam_flash_ctrl *fctrl,
 		struct cam_flash_query_cap_info flash_cap = {0};
 
 		CAM_DBG(CAM_FLASH, "CAM_QUERY_CAP");
-		flash_cap.slot_info  = fctrl->soc_info.index;
-		flash_cap.flash_type = soc_private->flash_type;
-		for (i = 0; i < fctrl->flash_num_sources; i++) {
-			flash_cap.max_current_flash[i] =
-				soc_private->flash_max_current[i];
-			flash_cap.max_duration_flash[i] =
-				soc_private->flash_max_duration[i];
-		}
-
-		for (i = 0; i < fctrl->torch_num_sources; i++)
-			flash_cap.max_current_torch[i] =
-				soc_private->torch_max_current[i];
-
+		flash_cap.slot_info = fctrl->soc_info.index;
+		cam_flash_populate_query_current(fctrl, &flash_cap, soc_private);
 		if (copy_to_user(u64_to_user_ptr(cmd->handle),
 			&flash_cap, sizeof(struct cam_flash_query_cap_info))) {
 			CAM_ERR(CAM_FLASH, "Failed Copy to User");
