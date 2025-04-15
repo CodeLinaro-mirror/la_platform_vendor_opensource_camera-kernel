063e37565a4069c9e15d9106722347165411166a
Stark Lin <quic_starlin@quicinc.com>
2025-04-15 16:12:42 +0800

    msm: camera: sensor: Add param validation before memcpy
    This change adds param validation before copying the cmd_buf.
    
    CRs-Fixed: 4056616
    Change-Id: Ied2071ab6bc89911a4eae9e1514122d9a13c2e5f
    Signed-off-by: Stark Lin <quic_starlin@quicinc.com>
    (cherry picked from commit 1cb2fc4624300737a7666fde5c8704d8502a7f22)

--- drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
+++ drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/kernel.h>
@@ -1376,24 +1376,23 @@ int32_t cam_sensor_update_power_settings(void *cmd_buf,
 	void *ptr = NULL, *scr;
 	struct common_header *cmm_hdr = NULL;
 	struct cam_cmd_power *pwr_cmd =
-		kzalloc(cmd_buf_len, GFP_KERNEL);
+		kzalloc(sizeof(struct cam_cmd_power), GFP_KERNEL);
 	if (!pwr_cmd) {
         CAM_DBG(CAM_SENSOR, "pwr_cmd memory allocation failed!");
 		return -ENOMEM;
 	}
-	memcpy(pwr_cmd, cmd_buf, cmd_buf_len);
-        ptr = pwr_cmd;
-	cmm_hdr = (struct common_header *)pwr_cmd;
 
-	if (!pwr_cmd || !cmd_length || cmd_buf_len < (size_t)cmd_length ||
+	if (!cmd_length || cmd_buf_len < (size_t)cmd_length ||
 		cam_sensor_validate(pwr_cmd, cmd_buf_len)) {
-		CAM_ERR(CAM_SENSOR, "Invalid Args: pwr_cmd %pK, cmd_length: %d",
-			pwr_cmd, cmd_length);
+		CAM_ERR(CAM_SENSOR, "Invalid Args: cmd_length: %d cmd_buf_len %d",
+			cmd_length, cmd_buf_len);
 		rc = -EINVAL;
 		goto free_power_command;
 	}
 
         memcpy(pwr_cmd, cmd_buf, sizeof(struct cam_cmd_power));
+        ptr = pwr_cmd;
+	cmm_hdr = (struct common_header *)pwr_cmd;
 
 	power_info->power_setting_size = 0;
 	power_info->power_setting =
