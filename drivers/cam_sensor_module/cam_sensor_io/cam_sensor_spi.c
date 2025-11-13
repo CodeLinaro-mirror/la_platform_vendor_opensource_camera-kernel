1e08f99d6fd7ae5c9c1ea722c50b97768e082def
Shadul Shaikh <shaduls@qti.qualcomm.com>
2025-11-13 19:25:47 +0530

    msm: camera: sensor: Correct format specifiers in prints
    Fixes all incorrect format specifiers used in print
    statements across the camera sensor drivers.
    
    CRs-Fixed:
    Change-Id: Ieae87a93fba035c6c771a270804e4bc4b3c70c06
    Signed-off-by: Shadul Shaikh <shaduls@qti.qualcomm.com>
    Signed-off-by: Prateek Pallav <ppallav@qti.qualcomm.com>

--- drivers/cam_sensor_module/cam_sensor_io/cam_sensor_spi.c
+++ drivers/cam_sensor_module/cam_sensor_io/cam_sensor_spi.c
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
+#include "cam_common_util.h"
 #include "cam_sensor_spi.h"
 #include "cam_debug_util.h"
 
