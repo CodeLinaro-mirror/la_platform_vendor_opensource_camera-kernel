1c46d84ddef19b1fe44394767e6880be643726fb
Chandan Kumar Jha <cjha@qti.qualcomm.com>
2025-12-01 20:05:51 +0530

    msm: camera: cpas: Fix CPAS DT node parsing
    Camera DLKM probe failed during node-tree parsing with FIT runtime
    overlays because of_find_node_by_name() searches recursively and could
    return nodes outside the expected hierarchy, leading to incorrect
    references and cleanup issues.
    Use of_get_child_by_name() to restrict lookup to immediate children
    under curr_node, ensuring predictable hierarchy handling and preventing
    invalid node references.
    
    This cahnge Prevents refcount underflow and node deletion during
    cleanup when overlays are applied at runtime.
    
    Change-Id: I472a1329a0389c15f76d997f7cb977796fd9bec0
    Signed-off-by: Chandan Kumar Jha <cjha@qti.qualcomm.com>

--- camera_kt/drivers/cam_cpas/cam_cpas_soc.c
+++ camera_kt/drivers/cam_cpas/cam_cpas_soc.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/device.h>
@@ -262,11 +262,14 @@ static int cam_cpas_parse_node_tree(struct cam_cpas *cpas_core,
 				&curr_node_ptr->merge_type);
 
 			curr_node_ptr->axi_port_idx = -1;
-			mnoc_node = of_find_node_by_name(curr_node,
+			mnoc_node = of_get_child_by_name(curr_node,
 				"qcom,axi-port-mnoc");
 			if (mnoc_node) {
-				if (mnoc_idx >= CAM_CPAS_MAX_AXI_PORTS)
+				if (mnoc_idx >= CAM_CPAS_MAX_AXI_PORTS) {
+					CAM_ERR(CAM_CPAS,
+						"Invalid axi mnoc idx %d", mnoc_idx);
 					return -EINVAL;
+				}
 
 				cpas_core->axi_port[mnoc_idx].axi_port_node
 					= mnoc_node;
