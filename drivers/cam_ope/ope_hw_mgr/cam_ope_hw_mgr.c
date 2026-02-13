b2d8f93f8fbacca08f98faf5fd3caa60e6e131bf
Parag Singhal <parsin@qti.qualcomm.com>
2026-02-13 11:58:52 +0530

    msm: camera: ope: Fix OOB write and memory leak in OPE driver
    Ensure the command buffer size does not exceed the remaining
    buffer length, preventing out‑of‑bound.
    
    Limit the number of generic command buffers to one per packet
    to prevent memory leaks.
    
    CRs-Fixed: 4384773
    CRs-Fixed: 4384796
    Change-Id: I855fcbb00094fd72362c7a3d356491c99306f70a
    Signed-off-by: Parag Singhal <parsin@qti.qualcomm.com>
    (cherry picked from commit aed545b1c2457300df65ccb58c63fa9e165272ea)

--- drivers/cam_ope/ope_hw_mgr/cam_ope_hw_mgr.c
+++ drivers/cam_ope/ope_hw_mgr/cam_ope_hw_mgr.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/uaccess.h>
@@ -2219,7 +2219,7 @@ static int cam_ope_mgr_process_cmd_buf_req(struct cam_ope_hw_mgr *hw_mgr,
 					ope_request->ope_kmd_buf.offset =
 						cmd_buf->offset;
 					ope_request->ope_kmd_buf.size =
-						cmd_buf->size;
+						cmd_buf->length;
 					is_kmd_buf_valid = true;
 					CAM_DBG(CAM_OPE, "kbuf:%x io:%x cdm:%x",
 					ope_request->ope_kmd_buf.cpu_addr,
@@ -2297,6 +2297,7 @@ static int cam_ope_mgr_process_cmd_desc(struct cam_ope_hw_mgr *hw_mgr,
 	struct cam_cmd_buf_desc *cmd_desc = NULL;
 	uintptr_t cpu_addr = 0;
 	struct cam_ope_request *ope_request;
+	int generic_cmd_buf_count = 0;
 
 	cmd_desc = (struct cam_cmd_buf_desc *)
 		((uint32_t *) &packet->payload + packet->cmd_buf_offset/4);
@@ -2311,6 +2312,13 @@ static int cam_ope_mgr_process_cmd_desc(struct cam_ope_hw_mgr *hw_mgr,
 			cmd_desc[i].meta_data == OPE_CMD_META_GENERIC_BLOB)
 			continue;
 
+		if (generic_cmd_buf_count > 0) {
+			CAM_ERR(CAM_OPE, "Multiple generic command buffers not supported");
+			rc = -EINVAL;
+			goto end;
+		}
+
+		generic_cmd_buf_count++;
 		rc = cam_mem_get_cpu_buf(cmd_desc[i].mem_handle,
 			&cpu_addr, &len);
 		if (rc || !cpu_addr) {
