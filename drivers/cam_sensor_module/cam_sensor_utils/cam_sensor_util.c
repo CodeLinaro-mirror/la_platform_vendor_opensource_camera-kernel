7ffe41a277fe7e86118f9574fba886074da96311
Yanhong Wu <yanhwu@qti.qualcomm.com>
2026-04-19 16:49:36 +0800

    msm: camera: memmgr: Add missing calls of put buf to avoid leak
    This change add calls to put cpu buf in few scenarios.
    
    CRs-Fixed: 3578162
    Signed-off-by: Vikram Sharma <quic_vikramsa@quicinc.com>
    Signed-off-by: Nirmal Abraham <quic_c_nabrah@quicinc.com>
    Signed-off-by: Shivakumar Malke <quic_smalke@quicinc.com>
    (cherry picked from commit 47fb597026ca9eb314fc81e445e546995d962211)
    Signed-off-by: Pranav Sanwal <quic_psanwal@quicinc.com>
    
    Signed-off-by: Yanhong Wu <yanhwu@qti.qualcomm.com>
    Change-Id: Iab6aa0324b5072390b38df296c7acee00f5102a1
    Signed-off-by: Jianc Li <jiancli@qti.qualcomm.com>

--- drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
+++ drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/kernel.h>
@@ -313,6 +313,7 @@ static int32_t cam_sensor_get_io_buffer(
 			CAM_ERR(CAM_SENSOR,
 				"invalid size:io_cfg->offsets[0]: %d, buf_size: %d",
 				io_cfg->offsets[0], buf_size);
+			cam_mem_put_cpu_buf(io_cfg->mem_handle[0]);
 			return -EINVAL;
 		}
 		i2c_settings->read_buff =
@@ -358,6 +359,7 @@ int32_t cam_sensor_util_write_qtimer_to_io_buffer(
 			CAM_ERR(CAM_SENSOR,
 				"invalid size:io_cfg->offsets[0]: %d, buf_size: %d",
 				io_cfg->offsets[0], buf_size);
+			cam_mem_put_cpu_buf(io_cfg->mem_handle[0]);
 			return -EINVAL;
 		}
 
@@ -368,6 +370,7 @@ int32_t cam_sensor_util_write_qtimer_to_io_buffer(
 			CAM_ERR(CAM_SENSOR,
 				"not enough size for qtimer, target_size:%d",
 				target_size);
+			cam_mem_put_cpu_buf(io_cfg->mem_handle[0]);
 			return -EINVAL;
 		}
 
@@ -378,12 +381,12 @@ int32_t cam_sensor_util_write_qtimer_to_io_buffer(
 		}
 
 		memcpy((void *)target_buf, &qtime_ns, sizeof(uint64_t));
+		cam_mem_put_cpu_buf(io_cfg->mem_handle[0]);
 	} else {
 		CAM_ERR(CAM_SENSOR, "Invalid direction: %d",
 			io_cfg->direction);
 		rc = -EINVAL;
 	}
-	cam_mem_put_cpu_buf(io_cfg->mem_handle[0]);
 	return rc;
 }
 
@@ -559,6 +562,7 @@ int cam_sensor_i2c_command_parser(
 			(cmd_desc[i].offset >
 			(len_of_buff - sizeof(struct common_header)))) {
 			CAM_ERR(CAM_SENSOR, "buffer provided too small");
+			cam_mem_put_cpu_buf(cmd_desc[i].mem_handle);
 			return -EINVAL;
 		}
 		cmd_buf = (uint32_t *)generic_ptr;
@@ -567,6 +571,7 @@ int cam_sensor_i2c_command_parser(
 		remain_len -= cmd_desc[i].offset;
 		if (remain_len < cmd_desc[i].length) {
 			CAM_ERR(CAM_SENSOR, "buffer provided too small");
+			cam_mem_put_cpu_buf(cmd_desc[i].mem_handle);
 			return -EINVAL;
 		}
 
