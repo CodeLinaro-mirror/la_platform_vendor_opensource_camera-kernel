e06be78c627df43fe019f58cdc5ac1291e87ff24
Huayuan Yang <huayyang@qti.qualcomm.com>
2026-04-30 15:56:25 +0800

    ais-kernel: enable ais-kernel build
    Change-Id: Ifad9db9059528413acbf48ca4755d1798dad08de
    Signed-off-by: Huayuan Yang <huayyang@qti.qualcomm.com>

--- config/volcano.mk
+++ config/volcano.mk
@@ -7,6 +7,8 @@ CONFIG_SPECTRA_TFE := y
 CONFIG_SPECTRA_CRE := y
 CONFIG_SPECTRA_SENSOR := y
 CONFIG_CSF_2_5_SECURE_CAMERA := y
+CONFIG_MSM_AIS := y
+CONFIG_V4L2_LOOPBACK_V2 := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_SPECTRA_ISP=1
@@ -15,3 +17,5 @@ ccflags-y += -DCONFIG_SPECTRA_TFE=1
 ccflags-y += -DCONFIG_SPECTRA_CRE=1
 ccflags-y += -DCONFIG_SPECTRA_SENSOR=1
 ccflags-y += -DCONFIG_CSF_2_5_SECURE_CAMERA=1
+ccflags-y += -DCONFIG_MSM_AIS=1
+ccflags-y += -DCONFIG_V4L2_LOOPBACK_V2=1
