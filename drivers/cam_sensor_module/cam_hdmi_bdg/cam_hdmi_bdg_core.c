d7e68f56dc3b8c5fd29290065479cef8be837142
Jianc Li <jiancli@qti.qualcomm.com>
2026-05-21 18:10:49 +0800

    msm:camera:sensor: Fix KW P1 issues for cam-kernel
    For fix the following two issue:
    32517:Array 'page_data' of size 32 may use index value(s) 32..UINT_MAX-1
    32518:Array 'page_data' of size 32 may use index value(s) 32..UINT_MAX-1
    
    Signed-off-by: Zun Qiao <quic_zqiao@quicinc.com>
    
    Change-Id: Ia79bc713fe96206ca515e614102b11ec0349b100
    Signed-off-by: Jianc Li <jiancli@qti.qualcomm.com>

--- drivers/cam_sensor_module/cam_hdmi_bdg/cam_hdmi_bdg_core.c
+++ drivers/cam_sensor_module/cam_hdmi_bdg/cam_hdmi_bdg_core.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
- * Copyright (c) 2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 #include <linux/module.h>
 #include <linux/firmware.h>
@@ -715,7 +714,7 @@ static int lt6911_fw_read_back(struct cam_sensor_ctrl_t *s_ctrl, u8 *buff, int s
 		addr += 32;
 	}
 
-	if(rest_data != 0) {
+	if((rest_data > 0) && (rest_data < 32)) {
 		memset(page_data, 0x0, rest_data);
 		rc = lt6911_flash_read_addr_set(s_ctrl, addr);
 		if (rc < 0)
